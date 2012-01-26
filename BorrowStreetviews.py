53c1717245dc7bd383bd277fc39fc2dfcdaa366f
David Stolarsky <david.stolarsky@gmail.com>
2012-01-26 14:25:15 -0500

    spherical projection for aspect calculation

--- BorrowStreetviews.py
+++ BorrowStreetviews.py
@@ -6,6 +6,38 @@ import json
 import pprint
 import time, calendar
 from datetime import timedelta
+import math
+
+def distance_on_spherical_earth(lat1, long1, lat2, long2):
+
+    # Convert latitude and longitude to 
+    # spherical coordinates in radians.
+    degrees_to_radians = math.pi/180.0
+        
+    # phi = 90 - latitude
+    phi1 = (90.0 - lat1)*degrees_to_radians
+    phi2 = (90.0 - lat2)*degrees_to_radians
+        
+    # theta = longitude
+    theta1 = long1*degrees_to_radians
+    theta2 = long2*degrees_to_radians
+        
+    # Compute spherical distance from spherical coordinates.
+        
+    # For two locations in spherical coordinates 
+    # (1, theta, phi) and (1, theta, phi)
+    # cosine( arc length ) = 
+    #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+    # distance = rho * arc length
+    
+    cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
+           math.cos(phi1)*math.cos(phi2))
+    arc = math.acos( cos )
+
+    # Remember to multiply arc by the radius of the earth 
+    # in your favorite set of units to get length.
+    return arc * 6378100
+
 
 top    = float(sys.argv[1])
 left   = float(sys.argv[2])
@@ -15,8 +47,16 @@ right  = float(sys.argv[4])
 degree_width  = (right - left)
 degree_height = (top - bottom)
 
+center_x = left + (right - left) / 2.0
+center_y = bottom + (top - bottom) / 2.0
+
+meter_width  = distance_on_spherical_earth(center_y, left, center_y, right)
+meter_height = distance_on_spherical_earth(bottom, center_x, top, center_x)
+
+print (meter_width, meter_height)
+
 columns = int(sys.argv[5])
-rows = int(columns * degree_height / degree_width)
+rows = int(columns * meter_height / meter_width)
 
 name = sys.argv[6]
 
@@ -28,11 +68,14 @@ unix_time = now()
 
 zoom = 2
 
+print 'Session is: ' + name + ' ' + str(unix_time)
+
 #print rows
 #exit()
 
 print str(columns) + ' x ' + str(rows)
 print str(degree_width) + " x " + str(degree_height)
+
 
 def saveMetadataAndDownloadTiles(panoJSON, column, row):
    pano = json.loads(panoJSON)
@@ -80,7 +123,7 @@ for x in range(0, columns):
          sys.stdout.write('=')
       for p in range(int(progress * 50), 50):
          sys.stdout.write(' ')
-      sys.stdout.write('| ' + str(progress*100) + ' % Remaining: ' + str(int(hours)) + 'h ' + str(int(minutes)) + 'm ' + str(int(seconds)) + 's\r')
+      sys.stdout.write('| ' + str(int(progress*100)) + ' % Remaining: ' + str(int(hours)) + 'h ' + str(int(minutes)) + 'm ' + str(int(seconds)) + 's               \r')
       sys.stdout.flush()
 
 sys.stdout.write('\n')
